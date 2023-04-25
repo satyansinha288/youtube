2a3e5a452692ffd68790be829101cb72f71b551b
satyansinha288 <sinhasatyan5152@gmail.com>
2023-04-25 18:31:14 +0530

    completed Home page and ListItems

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,37 +1,70 @@
+// @ts-nocheck
 import "./list.scss"
-import React from "react"
+import React, { useRef } from "react"
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io"
 import ListItem from "../listItem/ListItem"
 
-export default function List() {
-  return (
-    <div className="list">
-      <span className="listTitle">Continue to watch </span>
-      <div className="wrapper">
-        <IoIosArrowBack />
-        <div className="container">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-        </div>
+import { useState } from "react"
+// import { Directions } from "@material-ui/icons"
 
-        <IoIosArrowForward />
+export default function List() {
+  const [slideNumber, setSlideNumber] = useState(0)
+  const [isMoved, setIsMoved] = useState(false)
+
+  const listRef = useRef()
+
+  //Pure React Slider
+  const handleClick = (direction) => {
+    setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x - 50
+
+    if (listRef.current) {
+      if (direction === "left" && slideNumber > 0) {
+        setSlideNumber(slideNumber - 1)
+        listRef.current.style.transform = `translateX(${230 + distance}px)`
+      } else if (direction === "right" && slideNumber < 5) {
+        setSlideNumber(slideNumber + 1)
+        listRef.current.style.transform = `translateX(${-230 + distance}px)`
+      }
+    }
+
+    console.log(distance)
+  }
+
+  return (
+    <>
+      <div className="list">
+        <span className="listTitle">Continue to watch </span>
+        <div className="wrapper">
+          <IoIosArrowBack
+            className="sliderArrow left"
+            onClick={() => handleClick("left")}
+            style={{
+              display: !isMoved && "none",
+            }}
+          />
+          <div ref={listRef} className="container">
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+            <ListItem />
+          </div>
+
+          <IoIosArrowForward
+            className="sliderArrow right"
+            onClick={() => handleClick("right")}
+            // style={{ color: "red", fontSize: "50px" }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,6 +1,36 @@
 import React from "react"
 import "./listItem.scss"
+import { BsFillPlayFill } from "react-icons/bs"
+import { MdAdd } from "react-icons/md"
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai"
 
 export default function ListItem() {
-  return <div className="listItem">item</div>
+  return (
+    <div className="listItem">
+      <img
+        src="https://letstalkcinemamovie.files.wordpress.com/2020/08/peaky-blinders.jpg"
+        alt=""
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <BsFillPlayFill />
+          <MdAdd />
+          <AiOutlineLike />
+          <AiOutlineDislike />
+        </div>
+        <div className="itemTopInfo">
+          <span>1hrs 34mins</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+          officia blanditiis dolores similique voluptatem facilis numquam
+          corrupti esse nisi! Vitae deserunt est molestiae quod debitis
+          quibusdam saepe laborum nobis quo.
+        </div>
+        <div className="genre">Action</div>
+      </div>
+    </div>
+  )
 }
